bb700caa35e61f6ba710e56f9c73b9d26708b2fa
Rakshith <rakshithvishnu5614@gmail.com>
2023-10-26 20:56:03 +0530

    add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -18,6 +18,16 @@ app.use(express.json())
 app.listen(PORT, () => {
     console.log(`Server listening on port http://localhost:${PORT}`)
 })
-
+//middlewares
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode: statusCode
+    })
+})
